7dad0185265fcfb2c05bb71356ff496630e95c8a
Davide Guidi <davide@planetcom.co.uk>
2021-01-05 16:28:42 +0000

    test version

--- scripts/add-gemian-repos.sh
+++ scripts/add-gemian-repos.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 rm -f /etc/resolv.conf
+echo "93.189.6.11 gemian-buster.thinkglobally.org" >> /etc/hosts
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
 echo "deb [trusted=yes] http://gemian.thinkglobally.org/buster/ buster main" >> /etc/apt/sources.list.d/gemian.list
@@ -16,5 +17,4 @@ echo "Package: *" >> /etc/apt/preferences.d/gemian.pref
 echo "Pin: origin gemian-buster.thinkglobally.org" >> /etc/apt/preferences.d/gemian.pref
 echo "Pin-Priority: 2000" >> /etc/apt/preferences.d/gemian.pref
 
-curl http://gemian.thinkglobally.org/archive-key.asc | apt-key add -
-curl http://gemian-buster.thinkglobally.org/gemian-buster-key.asc | apt-key add -
+curl https://gemian.thinkglobally.org/archive-key.asc | apt-key add -
